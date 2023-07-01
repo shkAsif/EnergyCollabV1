69701381304f45cdd5022fe90e1011e51ae49676
aasifshk@gmail.com <aasifshk@gmail.com>
2023-07-01 19:03:18 +0530

    Add SendAsync

---
+++ EnergyCollab.Web/Service/IService/IBaseService.cs
@@ -0,0 +1,9 @@
+﻿using EnergyCollab.Web.Models;
+
+namespace EnergyCollab.Web.Service.IService
+{
+    public interface IBaseService
+    {
+        Task<ResponseDto?> SendAsync(RequestDto requestDto, bool withBearer = true);
+    }
+}
---
+++ EnergyCollab.Web/Service/IService/ITokenProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace EnergyCollab.Web.Service.IService
+{
+    public interface ITokenProvider
+    {
+        void SetToken(string token);
+        string? GetToken();
+        void ClearToken();
+    }
+}

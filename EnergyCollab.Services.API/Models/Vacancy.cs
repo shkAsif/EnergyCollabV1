2f2bfc4c75d1d9d169e89b43fb9c0e37e5a1d39c
Atif Shk <atifworld121@gmail.com>
2023-07-07 00:11:33 +0530

    add Vacancy entity

---
+++ EnergyCollab.Services.API/Models/Vacancy.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Drawing;
+
+namespace EnergyCollab.Services.API.Models
+{
+    public class Vacancy
+    {
+        public int Id { get; set; }
+        public string JobTitle { get; set; } = string.Empty;
+        public string Responsibilities { get; set; } = string.Empty;
+        public string ScopeofResponsibilities { get; set; } = string.Empty;
+        public string EntryRequirements { get; set; }
+        public string AdditionalInformation { get; set; } = string.Empty;
+        public bool Visa { get; set; }
+        public bool ActivationStatus { get; set; }
+        public bool IsActive { get; set; }
+        public int VacancyAdvertLength { get; set; }
+        public int IndustryExperience { get; set; }
+        public int Salary { get; set; }
+        public string Education { get; set; } = string.Empty;
+        public string ExperienceSummary { get; set; }
+        public Category EmpCategory { get; set; }
+        public string SubCategory { get; set; } = string.Empty;
+
+    }
+
+    public enum Category
+    {
+        Contract,
+        FullTime,
+        PartTime
+    }
+
+}
